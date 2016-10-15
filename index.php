b4bb4b49f405ae4078bcf72bc154233ffd543471
Lukáš Unger <looky.msc@gmail.com>
2016-10-15 13:33:21 +0200

    wip

--- index.php
+++ index.php
@@ -7,8 +7,6 @@ require_once __DIR__ . '/vendor/autoload.php';
 use Stevenmaguire\Uber\Client;
 use GuzzleHttp\Client as GuzzleClient;
 
-const UBER_API = '';
-
 header('Content-Type: application/json; charset=utf-8');
 
 $fail = function ($reason, $code) {
@@ -26,23 +24,42 @@ foreach ($requiredQueryArgs as $arg) {
 	}
 }
 
+$accessToken = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzY29wZXMiOlsicHJvZmlsZSIsImhpc3RvcnkiLCJwbGFjZXMiLCJoaXN0b3J5X2xpdGUiXSwic3ViIjoiZTIyNWNiOGEtNDJhNy00YWI2LWExZGQtYzZmNzI4ZmMwZmViIiwiaXNzIjoidWJlci11czEiLCJqdGkiOiJjZGVkODk5Zi03YjU1LTRmZTUtODYxNy03OWZlMTM1NGUyZmUiLCJleHAiOjE0NzkxMjIxNjYsImlhdCI6MTQ3NjUzMDE2NSwidWFjdCI6InRObUhpZ0NWbmVnWGswOEZ5cHcydkJaa3JGTXVDTyIsIm5iZiI6MTQ3NjUzMDA3NSwiYXVkIjoiLXhJeTVfR2taQUdVWU9XQWs5QzItVV9jNHB2WjF1U2sifQ.C8B2lkAfeF8Ty1mVK_9LHao23UVTmnvIoODAa01E0NP9XSulpga0y5kYmffGoTnnYOWJgQoAC9YgZIlxscm1FlMjB1CmzKBPyKXpd666AFUo3Qj3NC3-vgM_Lia-w7ifD683nUIjkYer-ns-NsNIwR5lpKn8hYuTi-GG7q_IkMYSN5rS4IPmN2Z886W_5NB9saMQl6alVOqBAhzTiCH2wNx8BmX58NkYD-JOa67e0-kcD1DgcBJHq2O1kf56DD7oipdDAT1DgaHK1xYpxFyiDvujo_HG8eXUEaUxCLgYP5dIkT7k2u5MYxO2f9OFca7u5nznl6fEIP6ft7BLqnTjfA';
 $client = new Client([
-	'access_token' => 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzY29wZXMiOltdLCJzdWIiOiJlMjI1Y2I4YS00MmE3LTRhYjYtYTFkZC1jNmY3MjhmYzBmZWIiLCJpc3MiOiJ1YmVyLXVzMSIsImp0aSI6ImRjOTFmNzIxLTJkYWItNGY3MS1iNDM3LTY1ZmM4YTYyNjcyMSIsImV4cCI6MTQ3OTExNjMzNywiaWF0IjoxNDc2NTI0MzM2LCJ1YWN0Ijoic1RDWWJ6elpYWVdCN090UEREaGNvSUR5UFJVczFOIiwibmJmIjoxNDc2NTI0MjQ2LCJhdWQiOiIteEl5NV9Ha1pBR1VZT1dBazlDMi1VX2M0cHZaMXVTayJ9.MgzHM-bF-6UWTZQrtp8NZHJAfE0V-quXXPuvsQ_G-STMBmm937IQ6sF7Q75gApmPjuqqhTMVCjjKCYyFO0Twz-EWuJBEwpK8hIN0wsbc6FRFPlUsUNL2EMWQ0wZk6WJWwFXQJBbQl7uelclROOMCS9ZEFfnRT0EYl6niPWui1xiGq9rP9YQYMPbwLZZFzFm-kz3vBkk-r2wLTxUKd8pCGNxszqtKsr_wt-fq8yujVFbVnxkaoUt25tZmIWYGQ37s_lrkm9173yap517bfUSMFfLWhqqW8kHum5JXTmXD7eDeDYmjQ5Kk1krIc4i30jjX5_p11U-K168aIKkIxwnylQ',
+	'access_token' => $accessToken,
 	'server_token' => 'bWyJdHPZOtS3sIJq80Nh-0X-V3pS2YzMO27k04Ks',
 	'use_sandbox' => true,
 	'version' => 'v1',
 	'locale' => 'en_US',
 ]);
-$client->setHttpClient(new GuzzleClient(['defaults' => ['verify' => false]]));
+$client->setHttpClient($guzzle = new GuzzleClient(['defaults' => ['verify' => false]]));
 $client->setVersion('v1.2');
 
+$requestIds = [];
 try {
-	$history = $client->getHistory([]);
-	var_dump($history);
-	die;
+	$history = $client->getHistory(['limit' => 50]);
+	foreach ($history->history as $ride) {
+		$requestIds[] = $ride->request_id;
+	}
 
 } catch (\Exception $e) {
-	$fail($e->getMessage(), 500);
+	return $fail($e->getMessage(), 500);
+}
+
+foreach ($requestIds as $requestId) {
+	try {
+		$response = $guzzle->get('https://sandbox-api.uber.com/v1/requests/' . $requestId . '/receipt', [
+			'headers' => [
+				'Authorization' => 'Bearer ' . $accessToken,
+				'Accept-Language' => 'en_US',
+			],
+		]);
+		echo $response->getBody();
+		return;
+
+	} catch (\Exception $e) {
+		return $fail($e->getMessage(), 500);
+	}
 }
 
 return $fail('fail', 404);
