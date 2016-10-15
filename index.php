af4106e1352292eb528fdbe01d9431f41fe0f32b
Vojtech Uhlir <wojtyla@Vojtechs-MacBook-Pro.local>
2016-10-15 11:54:34 +0200

    uber lib7

--- index.php
+++ index.php
@@ -32,6 +32,13 @@ foreach ($requiredQueryArgs as $arg) {
 echo "params ok";
 
 
+$client = new Stevenmaguire\Uber\Client(array(
+    'access_token' => 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzY29wZXMiOltdLCJzdWIiOiJlMjI1Y2I4YS00MmE3LTRhYjYtYTFkZC1jNmY3MjhmYzBmZWIiLCJpc3MiOiJ1YmVyLXVzMSIsImp0aSI6ImRjOTFmNzIxLTJkYWItNGY3MS1iNDM3LTY1ZmM4YTYyNjcyMSIsImV4cCI6MTQ3OTExNjMzNywiaWF0IjoxNDc2NTI0MzM2LCJ1YWN0Ijoic1RDWWJ6elpYWVdCN090UEREaGNvSUR5UFJVczFOIiwibmJmIjoxNDc2NTI0MjQ2LCJhdWQiOiIteEl5NV9Ha1pBR1VZT1dBazlDMi1VX2M0cHZaMXVTayJ9.MgzHM-bF-6UWTZQrtp8NZHJAfE0V-quXXPuvsQ_G-STMBmm937IQ6sF7Q75gApmPjuqqhTMVCjjKCYyFO0Twz-EWuJBEwpK8hIN0wsbc6FRFPlUsUNL2EMWQ0wZk6WJWwFXQJBbQl7uelclROOMCS9ZEFfnRT0EYl6niPWui1xiGq9rP9YQYMPbwLZZFzFm-kz3vBkk-r2wLTxUKd8pCGNxszqtKsr_wt-fq8yujVFbVnxkaoUt25tZmIWYGQ37s_lrkm9173yap517bfUSMFfLWhqqW8kHum5JXTmXD7eDeDYmjQ5Kk1krIc4i30jjX5_p11U-K168aIKkIxwnylQ',
+    'server_token' => 'bWyJdHPZOtS3sIJq80Nh-0X-V3pS2YzMO27k04Ks',
+    'use_sandbox'  => true, // optional, default false
+    'version'      => 'v1', // optional, default 'v1'
+    'locale'       => 'en_US', // optional, default 'en_US'
+));
 
 echo "uber ok";
 
